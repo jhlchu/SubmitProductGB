9194fe1fcaff003787449d3340b6e82de652f840
jhlchu <jhlchu@gmail.com>
2019-08-25 01:42:33 -0700

    targest added

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
 const puppeteer = require('puppeteer');
 const dotenv = require('dotenv');
+
+const targets = require('./targets');
 dotenv.config();
 
-(async () => {
-	const browser = await puppeteer.launch();
+const scrap = async() => {
+	const browser = await puppeteer.launch({headless: true});
 	const page = await browser.newPage();
-	const pageURL = process.env.pageURL;
-	await page.goto(pageURL);
+
+	await page.goto(targets[0], {waitUntil: 'networkidle2'});
+	await page.screenshot({path: 'screenshot.png'});
+}
+
+const main = async () => {
+	const browser = await puppeteer.launch({headless: true});
+	const page    = await browser.newPage();
+	
+	await page.goto(process.env.pageURL, {waitUntil: 'networkidle2'});
 	
 	/* await page.click('.cta-signin'); */
 	await page.type('input[type="email"]', process.env.Email);
 	await page.click('#next');
-	await page.waitFor(10);
-	
-	
+	await page.waitFor(10);	
 	
 	await page.type('#Passwd', process.env.PD);
 	await page.click('#signIn');
-	/* await page.waitFor(1000); */
+	
 	await page.goto(process.env.productPage, {waitUntil: 'networkidle2'});
-	await page.click('div[jsname="a5yMmf"]');
+	await page.click('div[jsname="#yDmH0d"]');
 	await page.waitFor(10);
 	await page.screenshot({path: 'screenshot.png'});
-	
-
 
 
 	await browser.close();
+}
+scrap();
+//main();
 
-	/* 
-	div[jscontroller="VXdfxd"]
+/* 
+(async () => {
+	const browser = await puppeteer.launch({headless: false});
+	const page    = await browser.newPage();
 	
-	*/
+	await page.goto(process.env.pageURL, {waitUntil: 'networkidle2'});
+	
+	//await page.click('.cta-signin');
+	await page.type('input[type="email"]', process.env.Email);
+	await page.click('#next');
+	await page.waitFor(10);	
+	
+	await page.type('#Passwd', process.env.PD);
+	await page.click('#signIn');
+	
+	await page.goto(process.env.productPage, {waitUntil: 'networkidle2'});
+	await page.click('div[jsname="a5yMmf"]');
+	await page.waitFor(10);
+
+	await page.screenshot({path: 'screenshot.png'});
+
+	await browser.close();
+
+	//div[jscontroller="VXdfxd"]
 })();
+ */
